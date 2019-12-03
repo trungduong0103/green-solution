0efe6484e5de69bbe2f9551f93395e4b109e3e7d
trungduong0103 <trungduong0103@gmail.com>
2019-12-04 00:03:41 +0700

    environments files

---
+++ functions/functions/environments/emailTemplates.js
@@ -0,0 +1,1 @@
+module.exports.WELCOME_MESSAGE = "Chào mừng bạn tới Green Solution Vietnam!";
---
+++ functions/functions/environments/errorTemplates.js
@@ -0,0 +1,7 @@
+module.exports.CANNOT_EMPTY = "Không được trống";
+module.exports.INVALID_EMAIL = "Xin nhập Email hợp lệ.";
+module.exports.PASSWORD_LENGTH_INSUFFICIENT = "Mật khẩu phải có ít nhất 6 kí tự.";
+module.exports.PASSWORDS_MUST_MATCH = "Mật khẩu xác nhận phải  với mật khẩu.";
+module.exports.EMAIL_ALREADY_IN_USE = "Email này đã được sử dụng.";
+module.exports.INVALID_CREDENTIALS = "Email/Password không hợp lệ.";
+module.exports.UNIDENTIFIED_ERRORS = "Lỗi không xác định.";
