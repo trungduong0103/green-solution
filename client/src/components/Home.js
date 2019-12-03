c5789186a764e232ac5357bdaf683ba1aaeaf420
trungduong0103 <trungduong0103@gmail.com>
2019-12-03 20:50:35 +0700

    Change Grid To GridList

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,45 +1,32 @@
 import React, {Component} from 'react';
 import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button"
-import Grid from "@material-ui/core/Grid";
-import {Typography} from "@material-ui/core";
-import Paper from "@material-ui/core/Paper"
+
 import banner from "../assets/imgs/img1.jpg"
 import {Link} from "react-router-dom";
 import NavBar from "./NavBar";
 
+import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+
 const styles = {
-    // home_title: {
-    //     align: "center",
-    // },
-    homeBanner: {
-        // width: "650px",
-        // height: "550px",
-        width: "100%",
-        height: "550px",
-        backgroundAttachment: "fixed",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-    },
-    banner: {
-        maxWidth: "100%",
-        maxHeight: "100%"
-    },
     registerForm: {
-        height: "52.4%",
-        position: "absolute",
+        height: "100%",
         backgroundColor: "rgb(99,151,68)",
         padding: "60px 40px",
-        borderRadius: 0,
-
+        borderRadius: 0
     },
     signUpBtn: {
-        // width: "150px",
         fontSize: 20,
         textTransform: "none",
-        fontFamily:"'Quicksand', sans-serif;",
+        fontFamily: "'Quicksand', sans-serif;",
         backgroundColor: "rgb(53,117,58)",
-        color: "black",
+        color: "white",
         "&:hover": {
             backgroundColor: "black",
             color: "white",
@@ -55,7 +42,7 @@ const styles = {
         color: "white"
     },
     typoText: {
-        fontFamily:"'Quicksand', sans-serif;",
+        fontFamily: "'Quicksand', sans-serif;",
         fontSize: 17,
         color: "white"
     }
@@ -74,44 +61,41 @@ class Home extends Component {
 
             <div>
                 <NavBar/>
+                <GridList cols={2} cellHeight={600}>
+                    <GridListTile>
+                        <img src={banner} alt="bannerBackground"/>
+                    </GridListTile>
 
-                <Grid container>
-                    <Grid item>
-                        <Grid container className={classes.homeBanner}>
-                            <img className={classes.banner} src={banner} alt="bannerBackground"/>
-                        </Grid>
+                    <GridListTile>
+                        <Card className={classes.registerForm} elevation={0}>
+                            <CardContent>
+                                <Typography variant="h4" gutterBottom className={classes.typoTitle}>
+                                    Giới thiệu về Green Solution
+                                </Typography>
 
-                    </Grid>
+                                <Typography gutterBottom className={classes.typoText} paragraph>
+                                    Mục tiêu của Green Solution là nâng cao nhận thức về vấn đề rác và xả rác bừa bãi ở
+                                    Việt
+                                    Nam.
 
-                    <Grid item sm={4} md={4}>
-                        <Paper className={classes.registerForm} elevation={0}>
-                            <Typography variant="h4" gutterBottom className={classes.typoTitle}>
-                                Giới thiệu về Green Solution
-                            </Typography>
+                                </Typography>
 
-                            <Typography gutterBottom className={classes.typoText} paragraph>
-                                Mục tiêu của Green Solution là nâng cao nhận thức về vấn đề rác và xả rác bừa bãi ở Việt Nam.
+                                <Typography className={classes.typoText} paragraph>
+                                    Chúng tôi hiểu rằng, trong khi có nhiều vấn đề khác tác động đến môi trường, chúng
+                                    tôi
+                                    tập trung vào vấn nạn xả rác bởi tin rằng đây là yếu tố cơ bản nhất dẫn đến những
+                                    vấn đề
+                                    môi trường khác
+                                    và là nền tảng trong chương trình giáo dục về bảo vệ môi trường.
+                                </Typography>
 
-                            </Typography>
-
-                            <Typography className={classes.typoText} paragraph>
-                                Chúng tôi hiểu rằng, trong khi có nhiều vấn đề khác tác động đến môi trường, chúng tôi tập trung vào vấn nạn xả rác bởi tin rằng đây là yếu tố cơ bản nhất dẫn đến những vấn đề môi trường khác
-                                và là nền tảng trong chương trình giáo dục về bảo vệ môi trường.
-                            </Typography>
-
-                            <Typography className={classes.typoText} paragraph>
-                                Ở thời điểm hiện tại, tồ chức chúng tôi đang triển khai nhiều chiến dịch dọn dẹp môi trường với hy vọng có thể giáo dục và nâng cao ý thức của thế hệ trẻ về bảo vệ môi trường.
-                            </Typography>
-
-                            {/*<Typography paragraph className={classes.typoText}>*/}
-                            {/*    Clean Up Event owner has to be at least 18, but for participant, everyone is warmly welcomed.*/}
-                            {/*    Ideal places for Clean Up event are beaches, parks, shared outdoor spaces and so on.*/}
-                            {/*</Typography>*/}
-
-                            {/*<Typography className={classes.typoText} >*/}
-                            {/*    We will send you a starter kit once you finish registering.*/}
-                            {/*</Typography>*/}
-                            <Grid container justify="center">
+                                <Typography className={classes.typoText} paragraph>
+                                    Ở thời điểm hiện tại, tồ chức chúng tôi đang triển khai nhiều chiến dịch dọn dẹp môi
+                                    trường với hy vọng có thể giáo dục và nâng cao ý thức của thế hệ trẻ về bảo vệ môi
+                                    trường.
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
                                 <Button
                                     variant="outlined"
                                     className={classes.signUpBtn}
@@ -119,15 +103,13 @@ class Home extends Component {
                                     to="/register">
                                     Xem thêm
                                 </Button>
-                            </Grid>
-
-                        </Paper>
-                    </Grid>
-                </Grid>
+                            </CardActions>
+                        </Card>
+                    </GridListTile>
+                </GridList>
 
 
                 <Grid container>
-
 
                 </Grid>
 
