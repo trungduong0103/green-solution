ccb86e302eff3dc02ad89c82f7fa8ed5f30034a7
Nmt131199 <manhtrietvt@gmail.com>
2020-01-02 16:32:36 +0700

    Clean Up Detail + Searchbar

--- client/src/components/pages/home/Search.js
+++ client/src/components/pages/home/Search.js
@@ -3,13 +3,32 @@ import {connect} from "react-redux";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import Fab from "@material-ui/core/Fab";
 import SearchIcon from "@material-ui/icons/Search";
 import ClearIcon from '@material-ui/icons/Clear';
+import Fab from "@material-ui/core/Fab";
+
 import withStyles from "@material-ui/core/styles/withStyles";
 import {filterByKeyword, resetFilters} from "../../../redux/actions/LocationFilterActions";
 
-const styles = {};
+const styles = {
+    searchWrapper: {
+        padding: "1em 1em 1em 1em",
+        width: "100%",
+        margin: "10px 0"
+    },
+    input: {
+        fontFamily: "'Quicksand', sans-serif;",
+    },
+    searchBtn: {
+        padding: "0 20px",
+        color: "white",
+        backgroundColor: "rgb(99,151,68)",
+        fontFamily: "'Quicksand', sans-serif;",
+        "&:hover": {
+            backgroundColor: "rgb(99,151,68)",
+        },
+    }
+};
 
 class Search extends Component {
     constructor(props) {
@@ -36,7 +55,7 @@ class Search extends Component {
         const {classes} = this.props;
         const {keyword} = this.state;
         return (
-            <Grid item sm={6} style={{border: "1px solid black"}}>
+            <Grid item sm={6} className={classes.searchWrapper}>
                 <Grid container spacing={2}>
                     <Grid item sm={8}>
                         <TextField
@@ -45,21 +64,28 @@ class Search extends Component {
                             name="keyword"
                             value={keyword}
                             id="input-with-icon-textfield"
-                            label="Location Search"
+                            label="Tìm kiếm"
                             fullWidth
                             onChange={this.handleChange}
                             InputProps={{
+                                className: classes.input,
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <SearchIcon />
+                                    </InputAdornment>
+                                ),
                                 endAdornment: (
                                     <InputAdornment position="start">
-                                        {!keyword ? <SearchIcon/> : <ClearIcon onClick={this.resetKeyword} cursor="pointer" />}
+                                        {!keyword ? "": <ClearIcon onClick={this.resetKeyword} cursor="pointer" />}
                                     </InputAdornment>
                                 ),
                             }}
+                            InputLabelProps={{className: classes.input}}
                         />
                     </Grid>
                     <Grid item sm={4}>
-                        <Fab onClick={this.handleSearch} variant="extended">
-                            Search
+                        <Fab onClick={this.handleSearch} variant="extended" className={classes.searchBtn}>
+                            Tìm
                         </Fab>
                     </Grid>
                 </Grid>
